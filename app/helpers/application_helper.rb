1f05190c8930920c0abc5d59ff26bd619b7d43ac
Cody Brooks <codyjamesbrooks@gmail.com>
2021-11-29 17:32:13 -0500

    configure results chart

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,33 @@
 module ApplicationHelper
   def chart_data(replies)
-    replies.group_by(&:value)
-           .transform_values(&:size)
-           .sort_by(&:first) 
+    no_result = { "😡" => 0, "🙁" => 0, "😐" => 0, "🙂" => 0, "😀" => 0 }
+    poll_result = replies.group_by(&:value)
+                        .transform_keys { |key| emoji_conversion(key) }
+                        .transform_values(&:size)
+    no_result.merge(poll_result).to_a
   end 
+  
+  def emoji_conversion(key)
+    case key
+    when "0"
+      "😡"
+    when "1"
+      "🙁"
+    when "2"
+      "😐"
+    when "3"
+      "🙂"
+    when "4"
+      "😀"
+    end
+  end 
+
+  # colors
+  # red #f94144
+  # orange #f9844a
+  # yellow #f9c74f
+  # green #90be6d
+  # blue #E577590
 
   def title_examples 
     ["a bunch of things", 
@@ -12,5 +36,14 @@ module ApplicationHelper
       "A great poll", 
       "My teams feelsings"
     ].sample
+  end 
+
+  def library_options 
+    { scales: {
+        xAxis: { 
+          ticks: { allowDecimals: false }
+        }
+      }
+    }
   end 
 end
