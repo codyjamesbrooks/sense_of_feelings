a8b1ca00b3703ace22045a685544274965621259
Cody Brooks <codyjamesbrooks@gmail.com>
2021-11-30 12:42:23 -0500

    update link/routes to use create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'static#index'
   
-  resources :polls, only: [:new, :create, :show, :update] do
+  resources :polls, only: [:create, :show, :update] do
     resources :replies, only: [:new, :create]
   end 
 end
